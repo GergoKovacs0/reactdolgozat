7d639a1a8bc6cac731d6278919aeaf0ae72e3c5d
GergoKovacs0 <c9kovger@vasvari.org>
2025-02-04 10:55:31 +0100

    added types

---
+++ src/types/Category.ts
@@ -0,0 +1,6 @@
+type Category = {
+    id:number;
+    nev:string
+}
+
+export default Category;
---
+++ src/types/Tree.ts
@@ -0,0 +1,14 @@
+import Category from "./Category";
+
+type Tree = {
+    nev:string;
+    ar: number;
+    id:number;
+    kategoria: Category;
+    kategoriaId: number;
+    kepUrl:string;
+    keszlet:number;
+    leiras: string;
+}
+
+export default Tree;
