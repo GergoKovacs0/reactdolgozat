1951e5b64aeb7b88947018fc516af0e0f23e9961
GergoKovacs0 <c9kovger@vasvari.org>
2025-02-04 10:55:37 +0100

    added router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+import Trees from './pages/Trees';
+import TreePage from './pages/Tree';
+import NewTree from './pages/NewTree';
+import NotFound from './pages/NotFound';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Trees />} />
+                <Route path="/tree/:id" element={<TreePage />} />
+                <Route path="/new-tree" element={<NewTree />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
 );
-
-root.render(<React.StrictMode>Hello World!</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
